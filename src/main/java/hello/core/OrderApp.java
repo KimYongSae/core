b84a9ffe0e77c9631d5c23254aa0dab42851eb58
KimYongSae <yonggale3@naver.com>
2024-01-24 13:45:55 +0900

    asdf

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -4,6 +4,7 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
@@ -16,6 +17,8 @@ public class OrderApp {
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
 
+        System.out.println("order = " + order);
     }
 }
